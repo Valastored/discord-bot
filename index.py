023de9dad030cb1fa7f32f119b94638e422f22b3
Vincent Nedelian <vincent.nedelian@gmail.com>
2024-08-27 10:40:23 +0200

    add index

---
+++ index.py
@@ -0,0 +1,19 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Intents
+
+intents = Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(description="Replies with pong!")
+async def badge(interaction: nextcord.Interaction):
+    await interaction.send("Hello!")
+
+bot.run('')
+
